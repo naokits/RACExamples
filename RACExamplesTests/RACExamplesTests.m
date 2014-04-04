55ab51c86031ab899721cd619986326a1232748c
Naoki Tsutsui <naokitsutsui@me.com>
2014-04-05 02:54:38 +0900

    テストを更新

--- RACExamplesTests/RACExamplesTests.m
+++ RACExamplesTests/RACExamplesTests.m
@@ -9,10 +9,20 @@
 #import <XCTest/XCTest.h>
 #import <TRVSMonitor/TRVSMonitor.h>
 
-@interface RACExamplesTests : XCTestCase
-@property (nonatomic, strong, getter=isPushed) NSNumber *pushd;
-@property (nonatomic, copy) NSString *name;
+/**
+*   UIが必要でないテストは基本的にここで行う
+*/
 
+@interface RACExamplesTests : XCTestCase
+
+@property (nonatomic, strong) UITextField *nameField;
+@property (nonatomic, copy) NSString *username;
+//@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *password;
+@property (nonatomic, copy) NSString *passwordConfirmation;
+@property (nonatomic, assign) BOOL isLoggedin;
+
+//@property (nonatomic, strong) NSArray *newNames;
 @end
 
 @implementation RACExamplesTests
@@ -21,6 +31,7 @@
 {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    
 }
 
 - (void)tearDown
@@ -36,18 +47,22 @@
 
 //}
 
+
+/*
+    usernameプロパティを監視し、変更がある毎にコンソールに表示します。
+ */
 - (void)testBasicObserv1
 {
-    __block BOOL waitingForBlock = YES;
-
-    [RACObserve(self, self.pushd) subscribeNext:^(NSNumber *x) {
-        NSLog(@"ページ移動");
-        waitingForBlock = NO;
+    [RACObserve(self, username) subscribeNext:^(NSString *newName) {
+        if (!newName) {
+//            XCTFail(@"%s :%@", __PRETTY_FUNCTION__, newName);
+        }
+        NSLog(@"変更された名前: %@", newName);
     }];
 
-    while(waitingForBlock) {
-        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
-    }
+    self.username = @"name1";
+    self.username = @"name2";
+    self.username = @"name3";
 
     NSLog(@"終了");
 }
@@ -55,53 +70,188 @@
 - (void)testBasicObserv2
 {
 //    TRVSMonitor *monitor = [[TRVSMonitor alloc] initWithExpectedSignalCount:2];
-    
-    [[RACObserve(self, name)
-      filter:^(NSString *newName) {
-          return [newName hasPrefix:@"Gill"];
-      }]
-     subscribeNext:^(NSString *x) {
-         NSLog(@"%@", x);
-     }];
-    
-    self.name = @"hoge";
-    self.name = @"Gillinghum";
-//    [monitor signal];
 
+    [[RACObserve(self, name)
+            filter:^(NSString *newName) {
+                return [newName hasPrefix:@"Gill"];
+            }]
+            subscribeNext:^(NSString *x) {
+                NSLog(@"%@", x);
+            }];
+
+    self.username = @"hoge";
+    self.username = @"Gillinghum";
+//    [monitor signal];
 //    [monitor wait];
     NSLog(@"終了");
 }
 
+
 /// シグナルの結合の例
 - (void)testCombineSignal
 {
-    __block BOOL waitingForBlock = YES;
-
     RACSignal *signal1 = @[@(1)].rac_sequence.signal;
     RACSignal *signal2 = @[@(2)].rac_sequence.signal;
-    
+
     [[RACSignal merge:@[signal1, signal2]]
-     subscribeCompleted:^{
-         NSLog(@"They are both done!");
-         XCTAssertTrue(YES, @"Should have been success!");
-     }];
+            subscribeCompleted:^{
+                NSLog(@"They are both done!");
+                XCTAssertTrue(YES, @"Should have been success!");
+            }];
 
     [signal1 subscribeNext:^(id x) {
         NSLog(@"signal1: %@", x);
     }];
-    
+
     [signal2 subscribeNext:^(id x) {
         NSLog(@"signal2: %@", x);
     }];
-    waitingForBlock = NO;
-
-
-    // Run the loop
-    while(waitingForBlock) {
-        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
-    }
 }
 
 
+//=============================================================================
+#pragma mark - シグナルの連鎖（チェイン）
+//=============================================================================
+
+/// hogeから始まる名前の場合だけコンソールに表示
+- (void)testChain
+{
+    [[RACObserve(self, username)
+            filter:^(NSString *newName) {
+                return [newName hasPrefix:@"hoge"]; }]
+            subscribeNext:^(NSString *newName) {
+                NSLog(@"%@", newName);
+            }];
+
+    self.username = @"hoge1";
+    self.username = @"jhoge2";
+    self.username = @"hoge3";
+}
+
+
+//=============================================================================
+#pragma mark - 重要でないテスト
+//=============================================================================
+
+- (void)testHogehoge
+{
+    NSArray *charArray = [@"A B C" componentsSeparatedByString:@" "];
+    RACSequence *charSequence = charArray.rac_sequence;
+    RACSequence *doubleCharSequence = [charSequence map:^id(NSString *value) {
+        return [value stringByAppendingString:value.lowercaseString];
+    }];
+    
+    RACSignal *signalFromDoubleCharSequence = [doubleCharSequence signal];
+    
+    [signalFromDoubleCharSequence subscribeNext:^(id x) {
+        NSLog(@"%@", x);
+    }];
+}
+
+
+// やりたいこと
+// 名前の配列があるとする。
+// 名前の配列を受け取って、名前の先頭に'@'を追加する処理を連続で実行する。
+- (RACSignal *)modifyName:(NSString *)name
+{
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        NSString *newName = [NSString stringWithFormat:@"@%@", name];
+        [subscriber sendNext:newName];
+        [subscriber sendCompleted];
+        return [RACDisposable disposableWithBlock:^{
+            //
+        }];
+    }];
+}
+
+
+- (void)testModifyName
+{
+    [[self modifyName:@"naokits"] subscribeNext:^(id x) {
+        NSLog(@"newname: %@", x);
+    }];
+}
+
+- (void)testJustReturnNameArray
+{
+    NSArray *array = @[@"name1", @"name2"];
+    RACSequence *list = [array rac_sequence];
+    RACSignal *signal = [[list map:^id(id value) {
+        return value;
+    }] signal];
+    
+    [signal subscribeNext:^(id x) {
+        NSLog(@"リストの内容: %@", x);
+    }];
+}
+
+
+- (void)testGenerateNewName
+{
+    NSArray *array = @[@"name1", @"name2"];
+    RACSequence *list = [array rac_sequence];
+    RACSignal *listSignal = [[list map:^id(id value) {
+        return value;
+    }] signal];
+    
+    [listSignal subscribeNext:^(id x) {
+        NSLog(@"リストの項目: %@", x);
+        [[self modifyName:@"naokits"] subscribeNext:^(id x) {
+            NSLog(@"newname: %@", x);
+        }];
+    }];
+}
+
+//- (RACSignal*)loadInformationSignal {
+//    RACSignal* coupon = [RACSignal defer:^RACSignal* { return [self.restHelper getCoupon]; }];
+//    RACSignal* packet = [RACSignal defer:^RACSignal* { return [self.restHelper getPacket]; }];
+//         
+//    RACSignal* sig = [[coupon concat:packet] collect];
+//    return self.restHelper.accessToken? sig : [[[self.restHelper authorize] catch:self.errorBlock] concat:sig];
+//}
+
+//=============================================================================
+#pragma mark - 状態の導出
+//=============================================================================
+
+/*
+
+Signalは状態の導出にも用いることができます。
+これまでのようにプロパティを監視し、その変化に応じて状態を示すための、プロパティを設定
+しなくとも、RACによってSignalや操作そのものが状態の特性を示すことが出来るのです。
+
+次の例では、パスワード入力欄とパスワード確認用の入力欄の値が同じであれば、createEnableed
+をYESとする、一方向のバインディングを作成します。
+
+RAC()はバインディングをいい感じに見せるためのマクロです。
+
+**+combineLatest: reduce:** は、Signalの配列を受け取り、各Signalの最新の値を元に
+Blockを実行し、その実行結果に応じて、新たに戻り値を送信する **RACSignal** を作成します。
+
+ */
+
+- (void)testBindState
+{
+    __block BOOL waitingForBlock = YES;
+
+    _isLoggedin = NO;
+    NSLog(@"完了前:%d", self.isLoggedin);
+
+    RAC(self, isLoggedin) =
+            [RACSignal combineLatest:@[RACObserve(self, password), RACObserve(self, passwordConfirmation)]
+                              reduce:^(NSString *password, NSString *passwordConfirm) {
+                                  return @([passwordConfirm isEqualToString:password]);
+                              }];
+
+    self.password = @"hogehoge";
+    self.passwordConfirmation = @"hogehoge";
+    waitingForBlock = NO;
+
+    while(waitingForBlock) {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
+    }
+
+    NSLog(@"完了後:%d", self.isLoggedin);
+}
 
 @end
